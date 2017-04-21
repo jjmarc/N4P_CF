024d03faba30f176c601b366b755efe3973e5541
jjmarc <jeanjacquesmarc8@gmail.com>
2017-04-21 14:43:24 +0200

    Update secret-gen.sh

--- scripts/secret-gen.sh
+++ scripts/secret-gen.sh
@@ -5,7 +5,7 @@ set -e
 export gopath=$(pwd)/gopath:$(pwd)/gopath/src/github.com/square/certstrap/Godeps/_workspace
 cd gopath/src/github.com/square/certstrap/
 ls
-./build
+go build
 ./bin/certstrap init --passphrase '' --common-name consulCA
 
 echo "fin test"
