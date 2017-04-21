bc53af137dbe458e64e209f18a7bb8aea9588c44
jjmarc <jeanjacquesmarc8@gmail.com>
2017-04-21 16:45:06 +0200

    Update secret-gen.sh

--- scripts/secret-gen.sh
+++ scripts/secret-gen.sh
@@ -7,11 +7,11 @@ dir=$(pwd)
 export GOPATH=$(pwd)/go
 cd go/src/github.com/square/certstrap/
 go build
-export PATH=$PATH:$(pwd)/certstrap
+export PATH=$PATH:$(pwd)
 ls
 cd ${dir}/updated-git/
 echo $PATH
-./certstrap init --passphrase '' --common-name consulCA
+certstrap init --passphrase '' --common-name consulCA
 
 git add .
 git commit -m "updated"
